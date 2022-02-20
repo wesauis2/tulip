d428b06545b809846d15782c63f23e853bdb3420
Wesley Augusto Isotton <wesleya.isotton@gmail.com>
2022-02-20 19:49:01 -0300

    Updates

---
+++ _next/static/BNalQQrj-nnHLqEglx2Bx/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/ebc642e66a7b804d.css","static/chunks/pages/index-31860e157d5a0612.js"],"/_error":["static/chunks/pages/_error-ace960bde90fa08e.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/LimTvHUR61BmNcCaqfaYI/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/ebc642e66a7b804d.css","static/chunks/pages/index-c5cecedfb81adec4.js"],"/_error":["static/chunks/pages/_error-ace960bde90fa08e.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
